88392ded1b96a3a90a353489ca95067de723db74
Samovich <valerysamovich@gmail.com>
2015-05-21 00:36:34 -0400

    Add Latin Translator app

---
+++ LatinTranslator/LatinTranslator/Form1.Designer.vb
@@ -0,0 +1,100 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnLeft = New System.Windows.Forms.Button()
+        Me.btnRight = New System.Windows.Forms.Button()
+        Me.btnCenter = New System.Windows.Forms.Button()
+        Me.btnAlignment = New System.Windows.Forms.Button()
+        Me.lblTranslator = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnLeft
+        '
+        Me.btnLeft.Location = New System.Drawing.Point(12, 12)
+        Me.btnLeft.Name = "btnLeft"
+        Me.btnLeft.Size = New System.Drawing.Size(75, 23)
+        Me.btnLeft.TabIndex = 0
+        Me.btnLeft.Text = "sinister"
+        Me.btnLeft.UseVisualStyleBackColor = True
+        '
+        'btnRight
+        '
+        Me.btnRight.Location = New System.Drawing.Point(93, 12)
+        Me.btnRight.Name = "btnRight"
+        Me.btnRight.Size = New System.Drawing.Size(75, 23)
+        Me.btnRight.TabIndex = 1
+        Me.btnRight.Text = "dexter"
+        Me.btnRight.UseVisualStyleBackColor = True
+        '
+        'btnCenter
+        '
+        Me.btnCenter.Location = New System.Drawing.Point(174, 12)
+        Me.btnCenter.Name = "btnCenter"
+        Me.btnCenter.Size = New System.Drawing.Size(75, 23)
+        Me.btnCenter.TabIndex = 2
+        Me.btnCenter.Text = "medium"
+        Me.btnCenter.UseVisualStyleBackColor = True
+        '
+        'btnAlignment
+        '
+        Me.btnAlignment.Location = New System.Drawing.Point(255, 12)
+        Me.btnAlignment.Name = "btnAlignment"
+        Me.btnAlignment.Size = New System.Drawing.Size(75, 23)
+        Me.btnAlignment.TabIndex = 3
+        Me.btnAlignment.Text = "alineacion"
+        Me.btnAlignment.UseVisualStyleBackColor = True
+        '
+        'lblTranslator
+        '
+        Me.lblTranslator.AutoSize = True
+        Me.lblTranslator.Location = New System.Drawing.Point(12, 48)
+        Me.lblTranslator.Name = "lblTranslator"
+        Me.lblTranslator.Size = New System.Drawing.Size(41, 13)
+        Me.lblTranslator.TabIndex = 4
+        Me.lblTranslator.Text = "English"
+        Me.lblTranslator.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(344, 262)
+        Me.Controls.Add(Me.lblTranslator)
+        Me.Controls.Add(Me.btnAlignment)
+        Me.Controls.Add(Me.btnCenter)
+        Me.Controls.Add(Me.btnRight)
+        Me.Controls.Add(Me.btnLeft)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnLeft As System.Windows.Forms.Button
+    Friend WithEvents btnRight As System.Windows.Forms.Button
+    Friend WithEvents btnCenter As System.Windows.Forms.Button
+    Friend WithEvents btnAlignment As System.Windows.Forms.Button
+    Friend WithEvents lblTranslator As System.Windows.Forms.Label
+
+End Class
---
+++ LatinTranslator/LatinTranslator/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+
+    Private Sub btnLeft_Click(sender As Object, e As EventArgs) Handles btnLeft.Click
+        lblTranslator.TextAlign = ContentAlignment.MiddleLeft
+    End Sub
+End Class
