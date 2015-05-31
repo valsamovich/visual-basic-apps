29ce86b1221b45d92a162904b5d5cee0dde3379c
Valery Samovich <valerysamovich@gmail.com>
2015-05-31 17:34:33 -0400

    Add Form Load procedures

--- apps/Room Charge Calculator/Room Charge Calculator/Form1.Designer.vb
+++ apps/Room Charge Calculator/Room Charge Calculator/Form1.Designer.vb
@@ -22,9 +22,17 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.SuspendLayout()
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
 End Class
--- apps/Room Charge Calculator/Room Charge Calculator/Form1.vb
+++ apps/Room Charge Calculator/Room Charge Calculator/Form1.vb
@@ -1,3 +1,12 @@
 ﻿Public Class Form1
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ' Get today's date from the system and display it.
+        lblDateToday.Text = Now.ToString("D")
+
+        ' Get the current time from the system and display it.
+        lblTimeToday.Text = Now.ToString("T")
+
+    End Sub
 End Class
