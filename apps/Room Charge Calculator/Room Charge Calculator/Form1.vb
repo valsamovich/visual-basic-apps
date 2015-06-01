94e9e9d7e437c2282fb2fe1c992e2e3440e5780a
Valery Samovich <valerysamovich@gmail.com>
2015-06-01 03:07:06 -0400

    Add Calculation for Room Charge and Additional Charge

--- apps/Room Charge Calculator/Room Charge Calculator/Form1.vb
+++ apps/Room Charge Calculator/Room Charge Calculator/Form1.vb
@@ -10,4 +10,30 @@
 
     End Sub
 
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        ' Declare variables for the calculations.
+        Dim decRoomCharges As Decimal        ' Room charges total
+        Dim decAddCharges As Decimal         ' Additional charges
+        Dim decSubtotal As Decimal           ' Subtotal
+        Dim decTax As Decimal                ' Tax
+        Dim decTotal As Decimal              ' Total of all charges
+        Const decTAX_RATE As Decimal = 0.08D ' Tax 
+
+        lblStatus.Text = String.Empty
+        Try
+            ' Calculate and display the room charges.
+            decRoomCharges = CDec(txtNights.Text) *
+                CDec(txtNightlyCharge.Text)
+            lblRoomCharges.Text = decRoomCharges.ToString("c")
+
+            ' Calculate and display the additional charges.
+            decAddCharges = CDec(txtRoomService.Text) +
+                CDec(txtTelephone.Text) +
+                CDec(txtMisc.Text)
+            lblAddCharges.Text = decAddCharges.ToString("c")
+        Catch
+            ' Error message
+            MessageBox.Show("All input must be valid numeric values.")
+        End Try
+    End Sub
 End Class
