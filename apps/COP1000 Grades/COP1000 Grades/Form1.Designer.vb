153da0935abbd04c0944bac3aa5fc9fa30159124
Valery Samovich <valerysamovich@gmail.com>
2015-06-07 19:12:20 -0400

    Add labels and text boxes for intro for user

--- apps/COP1000 Grades/COP1000 Grades/Form1.Designer.vb
+++ apps/COP1000 Grades/COP1000 Grades/Form1.Designer.vb
@@ -22,17 +22,289 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtIntroPost = New System.Windows.Forms.TextBox()
+        Me.lblIntroPost = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.txtChapter1Project = New System.Windows.Forms.TextBox()
+        Me.lblChapter1Project = New System.Windows.Forms.Label()
+        Me.txtChapter1Tutorial = New System.Windows.Forms.TextBox()
+        Me.lblChapter1Tutorial = New System.Windows.Forms.Label()
+        Me.txtChapter1Test = New System.Windows.Forms.TextBox()
+        Me.lblChapter1Test = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.lblChapter2Project = New System.Windows.Forms.Label()
+        Me.txtChapter2Tutorial = New System.Windows.Forms.TextBox()
+        Me.lblChapter2Tutorial = New System.Windows.Forms.Label()
+        Me.txtChapter2Test = New System.Windows.Forms.TextBox()
+        Me.lblChapter2Test = New System.Windows.Forms.Label()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.txtChapter3Tutorial = New System.Windows.Forms.TextBox()
+        Me.lblChapter3Tutorial = New System.Windows.Forms.Label()
+        Me.txtChapter3Test = New System.Windows.Forms.TextBox()
+        Me.lblChapter3Test = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(86, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(284, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Grade Average Calculator"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtIntroPost)
+        Me.GroupBox1.Controls.Add(Me.lblIntroPost)
+        Me.GroupBox1.Location = New System.Drawing.Point(133, 50)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(194, 60)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Introduction"
+        '
+        'txtIntroPost
+        '
+        Me.txtIntroPost.Location = New System.Drawing.Point(78, 24)
+        Me.txtIntroPost.Name = "txtIntroPost"
+        Me.txtIntroPost.Size = New System.Drawing.Size(100, 20)
+        Me.txtIntroPost.TabIndex = 1
+        '
+        'lblIntroPost
+        '
+        Me.lblIntroPost.AutoSize = True
+        Me.lblIntroPost.Location = New System.Drawing.Point(20, 27)
+        Me.lblIntroPost.Name = "lblIntroPost"
+        Me.lblIntroPost.Size = New System.Drawing.Size(52, 13)
+        Me.lblIntroPost.TabIndex = 0
+        Me.lblIntroPost.Text = "Intro Post"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.txtChapter1Project)
+        Me.GroupBox2.Controls.Add(Me.lblChapter1Project)
+        Me.GroupBox2.Controls.Add(Me.txtChapter1Tutorial)
+        Me.GroupBox2.Controls.Add(Me.lblChapter1Tutorial)
+        Me.GroupBox2.Controls.Add(Me.txtChapter1Test)
+        Me.GroupBox2.Controls.Add(Me.lblChapter1Test)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 124)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(139, 124)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Chapter 1"
+        '
+        'txtChapter1Project
+        '
+        Me.txtChapter1Project.Location = New System.Drawing.Point(54, 85)
+        Me.txtChapter1Project.Name = "txtChapter1Project"
+        Me.txtChapter1Project.Size = New System.Drawing.Size(70, 20)
+        Me.txtChapter1Project.TabIndex = 5
+        '
+        'lblChapter1Project
+        '
+        Me.lblChapter1Project.AutoSize = True
+        Me.lblChapter1Project.Location = New System.Drawing.Point(9, 88)
+        Me.lblChapter1Project.Name = "lblChapter1Project"
+        Me.lblChapter1Project.Size = New System.Drawing.Size(40, 13)
+        Me.lblChapter1Project.TabIndex = 4
+        Me.lblChapter1Project.Text = "Project"
+        '
+        'txtChapter1Tutorial
+        '
+        Me.txtChapter1Tutorial.Location = New System.Drawing.Point(54, 54)
+        Me.txtChapter1Tutorial.Name = "txtChapter1Tutorial"
+        Me.txtChapter1Tutorial.Size = New System.Drawing.Size(70, 20)
+        Me.txtChapter1Tutorial.TabIndex = 3
+        '
+        'lblChapter1Tutorial
+        '
+        Me.lblChapter1Tutorial.AutoSize = True
+        Me.lblChapter1Tutorial.Location = New System.Drawing.Point(6, 57)
+        Me.lblChapter1Tutorial.Name = "lblChapter1Tutorial"
+        Me.lblChapter1Tutorial.Size = New System.Drawing.Size(42, 13)
+        Me.lblChapter1Tutorial.TabIndex = 2
+        Me.lblChapter1Tutorial.Text = "Tutorial"
+        '
+        'txtChapter1Test
+        '
+        Me.txtChapter1Test.Location = New System.Drawing.Point(54, 25)
+        Me.txtChapter1Test.Name = "txtChapter1Test"
+        Me.txtChapter1Test.Size = New System.Drawing.Size(70, 20)
+        Me.txtChapter1Test.TabIndex = 1
+        '
+        'lblChapter1Test
+        '
+        Me.lblChapter1Test.AutoSize = True
+        Me.lblChapter1Test.Location = New System.Drawing.Point(20, 28)
+        Me.lblChapter1Test.Name = "lblChapter1Test"
+        Me.lblChapter1Test.Size = New System.Drawing.Size(28, 13)
+        Me.lblChapter1Test.TabIndex = 0
+        Me.lblChapter1Test.Text = "Test"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.TextBox1)
+        Me.GroupBox3.Controls.Add(Me.lblChapter2Project)
+        Me.GroupBox3.Controls.Add(Me.txtChapter2Tutorial)
+        Me.GroupBox3.Controls.Add(Me.lblChapter2Tutorial)
+        Me.GroupBox3.Controls.Add(Me.txtChapter2Test)
+        Me.GroupBox3.Controls.Add(Me.lblChapter2Test)
+        Me.GroupBox3.Location = New System.Drawing.Point(157, 124)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(139, 124)
+        Me.GroupBox3.TabIndex = 2
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Chapter 2"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(54, 85)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(70, 20)
+        Me.TextBox1.TabIndex = 5
+        '
+        'lblChapter2Project
+        '
+        Me.lblChapter2Project.AutoSize = True
+        Me.lblChapter2Project.Location = New System.Drawing.Point(9, 88)
+        Me.lblChapter2Project.Name = "lblChapter2Project"
+        Me.lblChapter2Project.Size = New System.Drawing.Size(40, 13)
+        Me.lblChapter2Project.TabIndex = 4
+        Me.lblChapter2Project.Text = "Project"
+        '
+        'txtChapter2Tutorial
+        '
+        Me.txtChapter2Tutorial.Location = New System.Drawing.Point(54, 54)
+        Me.txtChapter2Tutorial.Name = "txtChapter2Tutorial"
+        Me.txtChapter2Tutorial.Size = New System.Drawing.Size(70, 20)
+        Me.txtChapter2Tutorial.TabIndex = 3
+        '
+        'lblChapter2Tutorial
+        '
+        Me.lblChapter2Tutorial.AutoSize = True
+        Me.lblChapter2Tutorial.Location = New System.Drawing.Point(6, 57)
+        Me.lblChapter2Tutorial.Name = "lblChapter2Tutorial"
+        Me.lblChapter2Tutorial.Size = New System.Drawing.Size(42, 13)
+        Me.lblChapter2Tutorial.TabIndex = 2
+        Me.lblChapter2Tutorial.Text = "Tutorial"
+        '
+        'txtChapter2Test
+        '
+        Me.txtChapter2Test.Location = New System.Drawing.Point(54, 25)
+        Me.txtChapter2Test.Name = "txtChapter2Test"
+        Me.txtChapter2Test.Size = New System.Drawing.Size(70, 20)
+        Me.txtChapter2Test.TabIndex = 1
+        '
+        'lblChapter2Test
+        '
+        Me.lblChapter2Test.AutoSize = True
+        Me.lblChapter2Test.Location = New System.Drawing.Point(20, 28)
+        Me.lblChapter2Test.Name = "lblChapter2Test"
+        Me.lblChapter2Test.Size = New System.Drawing.Size(28, 13)
+        Me.lblChapter2Test.TabIndex = 0
+        Me.lblChapter2Test.Text = "Test"
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.txtChapter3Tutorial)
+        Me.GroupBox4.Controls.Add(Me.lblChapter3Tutorial)
+        Me.GroupBox4.Controls.Add(Me.txtChapter3Test)
+        Me.GroupBox4.Controls.Add(Me.lblChapter3Test)
+        Me.GroupBox4.Location = New System.Drawing.Point(302, 124)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(139, 124)
+        Me.GroupBox4.TabIndex = 2
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Chapter 3"
+        '
+        'txtChapter3Tutorial
+        '
+        Me.txtChapter3Tutorial.Location = New System.Drawing.Point(54, 54)
+        Me.txtChapter3Tutorial.Name = "txtChapter3Tutorial"
+        Me.txtChapter3Tutorial.Size = New System.Drawing.Size(70, 20)
+        Me.txtChapter3Tutorial.TabIndex = 3
+        '
+        'lblChapter3Tutorial
+        '
+        Me.lblChapter3Tutorial.AutoSize = True
+        Me.lblChapter3Tutorial.Location = New System.Drawing.Point(6, 57)
+        Me.lblChapter3Tutorial.Name = "lblChapter3Tutorial"
+        Me.lblChapter3Tutorial.Size = New System.Drawing.Size(42, 13)
+        Me.lblChapter3Tutorial.TabIndex = 2
+        Me.lblChapter3Tutorial.Text = "Tutorial"
+        '
+        'txtChapter3Test
+        '
+        Me.txtChapter3Test.Location = New System.Drawing.Point(54, 25)
+        Me.txtChapter3Test.Name = "txtChapter3Test"
+        Me.txtChapter3Test.Size = New System.Drawing.Size(70, 20)
+        Me.txtChapter3Test.TabIndex = 1
+        '
+        'lblChapter3Test
+        '
+        Me.lblChapter3Test.AutoSize = True
+        Me.lblChapter3Test.Location = New System.Drawing.Point(20, 28)
+        Me.lblChapter3Test.Name = "lblChapter3Test"
+        Me.lblChapter3Test.Size = New System.Drawing.Size(28, 13)
+        Me.lblChapter3Test.TabIndex = 0
+        Me.lblChapter3Test.Text = "Test"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(495, 521)
+        Me.ClientSize = New System.Drawing.Size(455, 521)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
         Me.Text = "COP1000 Grades"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox4.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents txtIntroPost As System.Windows.Forms.TextBox
+    Friend WithEvents lblIntroPost As System.Windows.Forms.Label
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents lblChapter1Tutorial As System.Windows.Forms.Label
+    Friend WithEvents txtChapter1Test As System.Windows.Forms.TextBox
+    Friend WithEvents lblChapter1Test As System.Windows.Forms.Label
+    Friend WithEvents txtChapter1Project As System.Windows.Forms.TextBox
+    Friend WithEvents lblChapter1Project As System.Windows.Forms.Label
+    Friend WithEvents txtChapter1Tutorial As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents lblChapter2Project As System.Windows.Forms.Label
+    Friend WithEvents txtChapter2Tutorial As System.Windows.Forms.TextBox
+    Friend WithEvents lblChapter2Tutorial As System.Windows.Forms.Label
+    Friend WithEvents txtChapter2Test As System.Windows.Forms.TextBox
+    Friend WithEvents lblChapter2Test As System.Windows.Forms.Label
+    Friend WithEvents GroupBox4 As System.Windows.Forms.GroupBox
+    Friend WithEvents txtChapter3Tutorial As System.Windows.Forms.TextBox
+    Friend WithEvents lblChapter3Tutorial As System.Windows.Forms.Label
+    Friend WithEvents txtChapter3Test As System.Windows.Forms.TextBox
+    Friend WithEvents lblChapter3Test As System.Windows.Forms.Label
 
 End Class
--- apps/COP1000 Grades/COP1000 Grades/Form1.vb
+++ apps/COP1000 Grades/COP1000 Grades/Form1.vb
@@ -1,3 +1,12 @@
-﻿Public Class Form1
+﻿' Written by Valery Samovich 
+' June 7, 2015 
+Option Strict On
+Option Explicit On
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 
 End Class
