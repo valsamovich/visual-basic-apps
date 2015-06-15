688d01db58e305c147b0c4e93d2e881396d2f0a2
Valery Samovich <valerysamovich@gmail.com>
2015-06-14 23:13:20 -0400

    Added Group boxes, labels, buttons, and textboxes.

--- apps/Long Distance Calls/Long Distance Calls/Form1.Designer.vb
+++ apps/Long Distance Calls/Long Distance Calls/Form1.Designer.vb
@@ -23,57 +23,63 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
-        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.radOffPeak = New System.Windows.Forms.RadioButton()
+        Me.radEvening = New System.Windows.Forms.RadioButton()
         Me.radDaytime = New System.Windows.Forms.RadioButton()
-        Me.rad = New System.Windows.Forms.RadioButton()
-        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.txtMinutes = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblTotalFee = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.lblStatus = New System.Windows.Forms.ToolStripStatusLabel()
         Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
         Me.GroupBox3.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'GroupBox1
         '
-        Me.GroupBox1.Controls.Add(Me.RadioButton3)
-        Me.GroupBox1.Controls.Add(Me.rad)
+        Me.GroupBox1.Controls.Add(Me.radOffPeak)
+        Me.GroupBox1.Controls.Add(Me.radEvening)
         Me.GroupBox1.Controls.Add(Me.radDaytime)
         Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(224, 100)
+        Me.GroupBox1.Size = New System.Drawing.Size(312, 106)
         Me.GroupBox1.TabIndex = 0
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Rate category"
         '
-        'GroupBox2
+        'radOffPeak
         '
-        Me.GroupBox2.Controls.Add(Me.TextBox1)
-        Me.GroupBox2.Controls.Add(Me.Label1)
-        Me.GroupBox2.Location = New System.Drawing.Point(12, 118)
-        Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(200, 100)
-        Me.GroupBox2.TabIndex = 0
-        Me.GroupBox2.TabStop = False
-        Me.GroupBox2.Text = "GroupBox2"
+        Me.radOffPeak.AutoSize = True
+        Me.radOffPeak.Location = New System.Drawing.Point(18, 69)
+        Me.radOffPeak.Name = "radOffPeak"
+        Me.radOffPeak.Size = New System.Drawing.Size(212, 17)
+        Me.radOffPeak.TabIndex = 2
+        Me.radOffPeak.Text = "Off-Peak (12:00 a.m. through 5:59 a.m.)"
+        Me.radOffPeak.UseVisualStyleBackColor = True
         '
-        'GroupBox3
+        'radEvening
         '
-        Me.GroupBox3.Controls.Add(Me.Label2)
-        Me.GroupBox3.Location = New System.Drawing.Point(12, 224)
-        Me.GroupBox3.Name = "GroupBox3"
-        Me.GroupBox3.Size = New System.Drawing.Size(200, 100)
-        Me.GroupBox3.TabIndex = 1
-        Me.GroupBox3.TabStop = False
-        Me.GroupBox3.Text = "GroupBox3"
+        Me.radEvening.AutoSize = True
+        Me.radEvening.Location = New System.Drawing.Point(18, 46)
+        Me.radEvening.Name = "radEvening"
+        Me.radEvening.Size = New System.Drawing.Size(209, 17)
+        Me.radEvening.TabIndex = 1
+        Me.radEvening.Text = "Evening (6:00 p.m. through 11:59 p.m.)"
+        Me.radEvening.UseVisualStyleBackColor = True
         '
         'radDaytime
         '
         Me.radDaytime.AutoSize = True
         Me.radDaytime.Checked = True
-        Me.radDaytime.Location = New System.Drawing.Point(10, 19)
+        Me.radDaytime.Location = New System.Drawing.Point(18, 23)
         Me.radDaytime.Name = "radDaytime"
         Me.radDaytime.Size = New System.Drawing.Size(202, 17)
         Me.radDaytime.TabIndex = 0
@@ -81,56 +87,112 @@ Partial Class Form1
         Me.radDaytime.Text = "Daytime (6:00 a.m. through 5:59 p.m.)"
         Me.radDaytime.UseVisualStyleBackColor = True
         '
-        'rad
+        'GroupBox2
         '
-        Me.rad.AutoSize = True
-        Me.rad.Location = New System.Drawing.Point(10, 42)
-        Me.rad.Name = "rad"
-        Me.rad.Size = New System.Drawing.Size(90, 17)
-        Me.rad.TabIndex = 1
-        Me.rad.Text = "RadioButton2"
-        Me.rad.UseVisualStyleBackColor = True
+        Me.GroupBox2.Controls.Add(Me.txtMinutes)
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 124)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(311, 72)
+        Me.GroupBox2.TabIndex = 0
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Call length"
         '
-        'RadioButton3
+        'txtMinutes
         '
-        Me.RadioButton3.AutoSize = True
-        Me.RadioButton3.Location = New System.Drawing.Point(10, 65)
-        Me.RadioButton3.Name = "RadioButton3"
-        Me.RadioButton3.Size = New System.Drawing.Size(90, 17)
-        Me.RadioButton3.TabIndex = 2
-        Me.RadioButton3.Text = "RadioButton3"
-        Me.RadioButton3.UseVisualStyleBackColor = True
+        Me.txtMinutes.Location = New System.Drawing.Point(180, 29)
+        Me.txtMinutes.Name = "txtMinutes"
+        Me.txtMinutes.Size = New System.Drawing.Size(100, 20)
+        Me.txtMinutes.TabIndex = 1
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(8, 23)
+        Me.Label1.Location = New System.Drawing.Point(32, 32)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.Size = New System.Drawing.Size(142, 13)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Label1"
+        Me.Label1.Text = "Enter the number of mintues:"
         '
-        'TextBox1
+        'GroupBox3
         '
-        Me.TextBox1.Location = New System.Drawing.Point(11, 48)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox1.TabIndex = 1
+        Me.GroupBox3.Controls.Add(Me.Label2)
+        Me.GroupBox3.Controls.Add(Me.lblTotalFee)
+        Me.GroupBox3.Location = New System.Drawing.Point(12, 202)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(311, 72)
+        Me.GroupBox3.TabIndex = 1
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Call fee"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(8, 16)
+        Me.Label2.Location = New System.Drawing.Point(84, 32)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(39, 13)
-        Me.Label2.TabIndex = 0
-        Me.Label2.Text = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(34, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Total:"
+        '
+        'lblTotalFee
+        '
+        Me.lblTotalFee.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTotalFee.Location = New System.Drawing.Point(129, 27)
+        Me.lblTotalFee.Name = "lblTotalFee"
+        Me.lblTotalFee.Size = New System.Drawing.Size(100, 23)
+        Me.lblTotalFee.TabIndex = 0
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(48, 290)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 2
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(129, 290)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 3
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(210, 290)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.lblStatus})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 330)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(336, 22)
+        Me.StatusStrip1.TabIndex = 5
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = False
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(121, 17)
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(318, 460)
+        Me.ClientSize = New System.Drawing.Size(336, 352)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
         Me.Controls.Add(Me.GroupBox3)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
@@ -142,17 +204,26 @@ Partial Class Form1
         Me.GroupBox2.PerformLayout()
         Me.GroupBox3.ResumeLayout(False)
         Me.GroupBox3.PerformLayout()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
-    Friend WithEvents RadioButton3 As System.Windows.Forms.RadioButton
-    Friend WithEvents rad As System.Windows.Forms.RadioButton
+    Friend WithEvents radOffPeak As System.Windows.Forms.RadioButton
+    Friend WithEvents radEvening As System.Windows.Forms.RadioButton
     Friend WithEvents radDaytime As System.Windows.Forms.RadioButton
     Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtMinutes As System.Windows.Forms.TextBox
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents lblTotalFee As System.Windows.Forms.Label
     Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents lblStatus As System.Windows.Forms.ToolStripStatusLabel
 
 End Class
--- apps/Long Distance Calls/Long Distance Calls/Form1.vb
+++ apps/Long Distance Calls/Long Distance Calls/Form1.vb
@@ -1,3 +1,14 @@
 ﻿Public Class Form1
 
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+    End Sub
 End Class
