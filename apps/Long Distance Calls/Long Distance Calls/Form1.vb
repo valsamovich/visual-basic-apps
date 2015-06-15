cd14bd440cb5641a42633546fe530e10c975dc4d
Valery Samovich <valerysamovich@gmail.com>
2015-06-15 00:02:28 -0400

    Add variables, the Clear and Exit handler events

--- apps/Long Distance Calls/Long Distance Calls/Form1.vb
+++ apps/Long Distance Calls/Long Distance Calls/Form1.vb
@@ -1,14 +1,37 @@
 ﻿Public Class Form1
 
     Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim intMinutes As Integer           ' Number of minutes
+        Dim dblTotalFee As Double           ' Total charge for a phone call
+        Dim blnInputOk As Boolean = True
 
+        ' Constants for rate per mintues
+        Const dblDAYTIME_FEE As Double = 0.07
+        Const dblEVENING_FEE As Double = 0.12
+        Const dblOFFPEAK_FEE As Double = 0.05
+
+        ' Validate and convert the number of minutes
     End Sub
 
     Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ' Reset the Daytime radio button.
+        radDaytime.Checked = True
 
+        ' Clear the number of minutes
+        txtMinutes.Clear()
+
+        ' Clear the total charge
+        lblTotalFee.Text = String.Empty
+
+        ' Clear the status message
+        lblStatus.Text = String.Empty
+
+        ' Give txtMinutes the focus.
+        txtMinutes.Focus()
     End Sub
 
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
-
+        ' Close the form.
+        Me.Close()
     End Sub
 End Class
