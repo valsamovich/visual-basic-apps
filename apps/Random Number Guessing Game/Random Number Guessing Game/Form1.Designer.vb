7f317d2a890855043c26f00655fb2abe65470401
Valery Samovich <valerysamovich@gmail.com>
2015-06-28 00:40:06 -0400

    Add UI Components

--- apps/Random Number Guessing Game/Random Number Guessing Game/Form1.Designer.vb
+++ apps/Random Number Guessing Game/Random Number Guessing Game/Form1.Designer.vb
@@ -22,9 +22,124 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lblNumberOfGuesses = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtGuessNumber = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnPlay = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblMessage = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(232, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Guess a number from 1 to 100 and click to play."
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lblNumberOfGuesses)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.txtGuessNumber)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 35)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(231, 98)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Game properties"
+        '
+        'lblNumberOfGuesses
+        '
+        Me.lblNumberOfGuesses.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblNumberOfGuesses.Location = New System.Drawing.Point(114, 54)
+        Me.lblNumberOfGuesses.Name = "lblNumberOfGuesses"
+        Me.lblNumberOfGuesses.Size = New System.Drawing.Size(100, 20)
+        Me.lblNumberOfGuesses.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(7, 58)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(101, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Number of guesses:"
+        '
+        'txtGuessNumber
+        '
+        Me.txtGuessNumber.Location = New System.Drawing.Point(114, 27)
+        Me.txtGuessNumber.Name = "txtGuessNumber"
+        Me.txtGuessNumber.Size = New System.Drawing.Size(100, 20)
+        Me.txtGuessNumber.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(30, 30)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(78, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Guess number:"
+        '
+        'btnPlay
+        '
+        Me.btnPlay.Location = New System.Drawing.Point(41, 175)
+        Me.btnPlay.Name = "btnPlay"
+        Me.btnPlay.Size = New System.Drawing.Size(75, 23)
+        Me.btnPlay.TabIndex = 2
+        Me.btnPlay.Text = "Play"
+        Me.btnPlay.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(132, 175)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblMessage
+        '
+        Me.lblMessage.Location = New System.Drawing.Point(12, 142)
+        Me.lblMessage.Name = "lblMessage"
+        Me.lblMessage.Size = New System.Drawing.Size(231, 23)
+        Me.lblMessage.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(255, 210)
+        Me.Controls.Add(Me.lblMessage)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnPlay)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Number Guess Game"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents lblNumberOfGuesses As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtGuessNumber As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents btnPlay As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents lblMessage As System.Windows.Forms.Label
 
 End Class
--- apps/Random Number Guessing Game/Random Number Guessing Game/Form1.vb
+++ apps/Random Number Guessing Game/Random Number Guessing Game/Form1.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Form1
 
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
 End Class
