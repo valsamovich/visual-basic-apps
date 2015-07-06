f696229999b486b2dc438e4131287d6cc0e4c4c8
Valery Samovich <valerysamovich@gmail.com>
2015-07-06 01:33:39 -0400

    Add properties for labels and buttons

--- apps/Joe's Automotive/Joe's Automotive/Form1.Designer.vb
+++ apps/Joe's Automotive/Joe's Automotive/Form1.Designer.vb
@@ -38,17 +38,25 @@ Partial Class Form1
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.GroupBox5 = New System.Windows.Forms.GroupBox()
-        Me.Label10 = New System.Windows.Forms.Label()
-        Me.Label9 = New System.Windows.Forms.Label()
-        Me.Label8 = New System.Windows.Forms.Label()
-        Me.Label7 = New System.Windows.Forms.Label()
+        Me.lblTotalFees = New System.Windows.Forms.Label()
+        Me.lblTaxOnParts = New System.Windows.Forms.Label()
+        Me.lblParts = New System.Windows.Forms.Label()
+        Me.lblServicesAndLabel = New System.Windows.Forms.Label()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
+        Me.btnCalculateTotal = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.Label18 = New System.Windows.Forms.Label()
         Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
         Me.GroupBox3.SuspendLayout()
@@ -58,11 +66,13 @@ Partial Class Form1
         '
         'GroupBox1
         '
+        Me.GroupBox1.Controls.Add(Me.Label12)
+        Me.GroupBox1.Controls.Add(Me.Label11)
         Me.GroupBox1.Controls.Add(Me.chkLubeJob)
         Me.GroupBox1.Controls.Add(Me.chkOilChange)
         Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(200, 73)
+        Me.GroupBox1.Size = New System.Drawing.Size(225, 73)
         Me.GroupBox1.TabIndex = 0
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Oil Change"
@@ -89,11 +99,13 @@ Partial Class Form1
         '
         'GroupBox2
         '
+        Me.GroupBox2.Controls.Add(Me.Label14)
+        Me.GroupBox2.Controls.Add(Me.Label13)
         Me.GroupBox2.Controls.Add(Me.chkTransmissionFlush)
         Me.GroupBox2.Controls.Add(Me.chkRadiatorFlush)
-        Me.GroupBox2.Location = New System.Drawing.Point(218, 12)
+        Me.GroupBox2.Location = New System.Drawing.Point(243, 12)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(200, 73)
+        Me.GroupBox2.Size = New System.Drawing.Size(225, 73)
         Me.GroupBox2.TabIndex = 0
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Flushes"
@@ -120,12 +132,15 @@ Partial Class Form1
         '
         'GroupBox3
         '
+        Me.GroupBox3.Controls.Add(Me.Label17)
+        Me.GroupBox3.Controls.Add(Me.Label16)
+        Me.GroupBox3.Controls.Add(Me.Label15)
         Me.GroupBox3.Controls.Add(Me.chkTireRotation)
         Me.GroupBox3.Controls.Add(Me.chkReplaceMuffler)
         Me.GroupBox3.Controls.Add(Me.chkInspection)
         Me.GroupBox3.Location = New System.Drawing.Point(12, 91)
         Me.GroupBox3.Name = "GroupBox3"
-        Me.GroupBox3.Size = New System.Drawing.Size(200, 97)
+        Me.GroupBox3.Size = New System.Drawing.Size(225, 97)
         Me.GroupBox3.TabIndex = 1
         Me.GroupBox3.TabStop = False
         Me.GroupBox3.Text = "Misc"
@@ -162,13 +177,14 @@ Partial Class Form1
         '
         'GroupBox4
         '
+        Me.GroupBox4.Controls.Add(Me.Label18)
         Me.GroupBox4.Controls.Add(Me.txtLabor)
         Me.GroupBox4.Controls.Add(Me.txtParts)
         Me.GroupBox4.Controls.Add(Me.Label2)
         Me.GroupBox4.Controls.Add(Me.Label1)
-        Me.GroupBox4.Location = New System.Drawing.Point(218, 91)
+        Me.GroupBox4.Location = New System.Drawing.Point(243, 91)
         Me.GroupBox4.Name = "GroupBox4"
-        Me.GroupBox4.Size = New System.Drawing.Size(200, 97)
+        Me.GroupBox4.Size = New System.Drawing.Size(225, 97)
         Me.GroupBox4.TabIndex = 2
         Me.GroupBox4.TabStop = False
         Me.GroupBox4.Text = "Parts and Labor"
@@ -207,128 +223,196 @@ Partial Class Form1
         '
         'GroupBox5
         '
-        Me.GroupBox5.Controls.Add(Me.Label10)
-        Me.GroupBox5.Controls.Add(Me.Label9)
-        Me.GroupBox5.Controls.Add(Me.Label8)
-        Me.GroupBox5.Controls.Add(Me.Label7)
+        Me.GroupBox5.Controls.Add(Me.lblTotalFees)
+        Me.GroupBox5.Controls.Add(Me.lblTaxOnParts)
+        Me.GroupBox5.Controls.Add(Me.lblParts)
+        Me.GroupBox5.Controls.Add(Me.lblServicesAndLabel)
         Me.GroupBox5.Controls.Add(Me.Label6)
         Me.GroupBox5.Controls.Add(Me.Label5)
         Me.GroupBox5.Controls.Add(Me.Label4)
         Me.GroupBox5.Controls.Add(Me.Label3)
-        Me.GroupBox5.Location = New System.Drawing.Point(12, 224)
+        Me.GroupBox5.Location = New System.Drawing.Point(12, 194)
         Me.GroupBox5.Name = "GroupBox5"
-        Me.GroupBox5.Size = New System.Drawing.Size(406, 100)
+        Me.GroupBox5.Size = New System.Drawing.Size(456, 139)
         Me.GroupBox5.TabIndex = 3
         Me.GroupBox5.TabStop = False
         Me.GroupBox5.Text = "Summary"
         '
-        'Label10
+        'lblTotalFees
         '
-        Me.Label10.AutoSize = True
-        Me.Label10.Location = New System.Drawing.Point(144, 55)
-        Me.Label10.Name = "Label10"
-        Me.Label10.Size = New System.Drawing.Size(45, 13)
-        Me.Label10.TabIndex = 7
-        Me.Label10.Text = "Label10"
+        Me.lblTotalFees.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTotalFees.Location = New System.Drawing.Point(221, 104)
+        Me.lblTotalFees.Name = "lblTotalFees"
+        Me.lblTotalFees.Size = New System.Drawing.Size(100, 20)
+        Me.lblTotalFees.TabIndex = 7
         '
-        'Label9
+        'lblTaxOnParts
         '
-        Me.Label9.AutoSize = True
-        Me.Label9.Location = New System.Drawing.Point(144, 42)
-        Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(39, 13)
-        Me.Label9.TabIndex = 6
-        Me.Label9.Text = "Label9"
+        Me.lblTaxOnParts.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTaxOnParts.Location = New System.Drawing.Point(221, 76)
+        Me.lblTaxOnParts.Name = "lblTaxOnParts"
+        Me.lblTaxOnParts.Size = New System.Drawing.Size(100, 20)
+        Me.lblTaxOnParts.TabIndex = 6
         '
-        'Label8
+        'lblParts
         '
-        Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(144, 29)
-        Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(39, 13)
-        Me.Label8.TabIndex = 5
-        Me.Label8.Text = "Label8"
+        Me.lblParts.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblParts.Location = New System.Drawing.Point(221, 48)
+        Me.lblParts.Name = "lblParts"
+        Me.lblParts.Size = New System.Drawing.Size(100, 20)
+        Me.lblParts.TabIndex = 5
         '
-        'Label7
+        'lblServicesAndLabel
         '
-        Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(144, 16)
-        Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(39, 13)
-        Me.Label7.TabIndex = 4
-        Me.Label7.Text = "Label7"
+        Me.lblServicesAndLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblServicesAndLabel.Location = New System.Drawing.Point(221, 20)
+        Me.lblServicesAndLabel.Name = "lblServicesAndLabel"
+        Me.lblServicesAndLabel.Size = New System.Drawing.Size(100, 20)
+        Me.lblServicesAndLabel.TabIndex = 4
         '
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(6, 55)
+        Me.Label6.Location = New System.Drawing.Point(158, 107)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(39, 13)
+        Me.Label6.Size = New System.Drawing.Size(57, 13)
         Me.Label6.TabIndex = 3
-        Me.Label6.Text = "Label6"
+        Me.Label6.Text = "Total Fees"
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(6, 42)
+        Me.Label5.Location = New System.Drawing.Point(143, 79)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(39, 13)
+        Me.Label5.Size = New System.Drawing.Size(72, 13)
         Me.Label5.TabIndex = 2
-        Me.Label5.Text = "Label5"
+        Me.Label5.Text = "Tax (on parts)"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(6, 29)
+        Me.Label4.Location = New System.Drawing.Point(184, 51)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.Size = New System.Drawing.Size(31, 13)
         Me.Label4.TabIndex = 1
-        Me.Label4.Text = "Label4"
+        Me.Label4.Text = "Parts"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(6, 16)
+        Me.Label3.Location = New System.Drawing.Point(126, 23)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.Size = New System.Drawing.Size(87, 13)
         Me.Label3.TabIndex = 0
-        Me.Label3.Text = "Label3"
+        Me.Label3.Text = "Services && Labor"
         '
-        'Button1
+        'btnCalculateTotal
         '
-        Me.Button1.Location = New System.Drawing.Point(45, 358)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(111, 23)
-        Me.Button1.TabIndex = 4
-        Me.Button1.Text = "Calculate Total"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnCalculateTotal.Location = New System.Drawing.Point(103, 347)
+        Me.btnCalculateTotal.Name = "btnCalculateTotal"
+        Me.btnCalculateTotal.Size = New System.Drawing.Size(111, 30)
+        Me.btnCalculateTotal.TabIndex = 4
+        Me.btnCalculateTotal.Text = "Calculate Total"
+        Me.btnCalculateTotal.UseVisualStyleBackColor = True
         '
-        'Button2
+        'btnClear
         '
-        Me.Button2.Location = New System.Drawing.Point(162, 358)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 5
-        Me.Button2.Text = "Clear"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.btnClear.Location = New System.Drawing.Point(220, 347)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 30)
+        Me.btnClear.TabIndex = 5
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
         '
-        'Button3
+        'btnExit
         '
-        Me.Button3.Location = New System.Drawing.Point(243, 358)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(75, 23)
-        Me.Button3.TabIndex = 6
-        Me.Button3.Text = "Exit"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.btnExit.Location = New System.Drawing.Point(301, 347)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 30)
+        Me.btnExit.TabIndex = 6
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(143, 20)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(46, 13)
+        Me.Label11.TabIndex = 2
+        Me.Label11.Text = "($26.00)"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(142, 43)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(46, 13)
+        Me.Label12.TabIndex = 3
+        Me.Label12.Text = "($18.00)"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(144, 42)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(46, 13)
+        Me.Label13.TabIndex = 2
+        Me.Label13.Text = "($80.00)"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(144, 20)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(46, 13)
+        Me.Label14.TabIndex = 3
+        Me.Label14.Text = "($30.00)"
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(144, 20)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(46, 13)
+        Me.Label15.TabIndex = 3
+        Me.Label15.Text = "($15.00)"
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.Location = New System.Drawing.Point(144, 43)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(52, 13)
+        Me.Label16.TabIndex = 4
+        Me.Label16.Text = "($100.00)"
+        '
+        'Label17
+        '
+        Me.Label17.AutoSize = True
+        Me.Label17.Location = New System.Drawing.Point(144, 66)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(46, 13)
+        Me.Label17.TabIndex = 5
+        Me.Label17.Text = "($20.00)"
+        '
+        'Label18
+        '
+        Me.Label18.AutoSize = True
+        Me.Label18.Location = New System.Drawing.Point(133, 57)
+        Me.Label18.Name = "Label18"
+        Me.Label18.Size = New System.Drawing.Size(88, 13)
+        Me.Label18.TabIndex = 4
+        Me.Label18.Text = "($20.00 per hour)"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(431, 420)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
+        Me.ClientSize = New System.Drawing.Size(481, 392)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculateTotal)
         Me.Controls.Add(Me.GroupBox5)
         Me.Controls.Add(Me.GroupBox4)
         Me.Controls.Add(Me.GroupBox3)
@@ -365,16 +449,24 @@ Partial Class Form1
     Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents GroupBox5 As System.Windows.Forms.GroupBox
-    Friend WithEvents Label10 As System.Windows.Forms.Label
-    Friend WithEvents Label9 As System.Windows.Forms.Label
-    Friend WithEvents Label8 As System.Windows.Forms.Label
-    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents lblTotalFees As System.Windows.Forms.Label
+    Friend WithEvents lblTaxOnParts As System.Windows.Forms.Label
+    Friend WithEvents lblParts As System.Windows.Forms.Label
+    Friend WithEvents lblServicesAndLabel As System.Windows.Forms.Label
     Friend WithEvents Label6 As System.Windows.Forms.Label
     Friend WithEvents Label5 As System.Windows.Forms.Label
     Friend WithEvents Label4 As System.Windows.Forms.Label
     Friend WithEvents Label3 As System.Windows.Forms.Label
-    Friend WithEvents Button1 As System.Windows.Forms.Button
-    Friend WithEvents Button2 As System.Windows.Forms.Button
-    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents btnCalculateTotal As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Label14 As System.Windows.Forms.Label
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents Label17 As System.Windows.Forms.Label
+    Friend WithEvents Label16 As System.Windows.Forms.Label
+    Friend WithEvents Label15 As System.Windows.Forms.Label
+    Friend WithEvents Label18 As System.Windows.Forms.Label
 
 End Class
