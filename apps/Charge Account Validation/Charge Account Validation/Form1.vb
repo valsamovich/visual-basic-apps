db104411eaaca87c22ed9ac64927adb2257bb4f9
Valery Samovich <valerysamovich@gmail.com>
2015-07-13 01:33:43 -0400

    Add comments

--- apps/Charge Account Validation/Charge Account Validation/Form1.vb
+++ apps/Charge Account Validation/Charge Account Validation/Form1.vb
@@ -5,7 +5,7 @@ Option Explicit On
 
 Public Class Form1
 
-    ' Declar array of valid numbers 
+    ' Declar array of valid account numbers. 
     Dim intNumbers() As Integer = {5658845, 4520125, 7895122, 8777541, 8451277, 1302850,
                                    8080152, 4562555, 5552012, 5050552, 7825877, 1250255,
                                    1005231, 6545231, 3852085, 7576651, 7881200, 4581002}
