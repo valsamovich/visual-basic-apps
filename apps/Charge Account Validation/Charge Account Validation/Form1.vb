6066da0cfaf8a90b7c6e95c31176f25401347d44
Valery Samovich <valerysamovich@gmail.com>
2015-07-12 23:50:38 -0400

    Add input validation function

--- apps/Charge Account Validation/Charge Account Validation/Form1.vb
+++ apps/Charge Account Validation/Charge Account Validation/Form1.vb
@@ -11,13 +11,41 @@ Public Class Form1
                                    1005231, 6545231, 3852085, 7576651, 7881200, 4581002}
 
     Private Sub btnValidate_Click(sender As Object, e As EventArgs) Handles btnValidate.Click
+        Dim intInput As Integer     ' To hold input
 
+        If inputIsValid() Then
+            ' Get the input value.
+            Integer.TryParse(txtInput.Text, intInput)
+
+
+        End If
     End Sub
 
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
         ' Close the form.
         Me.Close()
     End Sub
+
+    Function inputIsValid() As Boolean
+        ' Varialbe to temporary hold the input value.
+        Dim intInputTempValue As Integer
+
+        ' Try to convert the value entered by the user to a Integer.
+        ' If it will not convert, display an error message and return false. 
+        If Not Integer.TryParse(txtInput.Text, intInputTempValue) Then
+            MessageBox.Show("Enter a numeric value for the input.")
+            Return False
+        End If
+
+        ' Determine whether the value entered is negative.
+        ' If it is, display an error message and return false.
+        If intInputTempValue < 0 Then
+            MessageBox.Show("Enter a positive numeric value for the input.")
+        End If
+
+        ' If value is valid, return true.
+        Return True
+    End Function
 
     ' Sequential search to determine wheather a number appears in the array.
     Function isValid(ByVal intInput As Integer) As Boolean
