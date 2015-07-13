fa90b4f1849dde5f728311d0826fbd302456ca75
Valery Samovich <valerysamovich@gmail.com>
2015-07-12 22:05:44 -0400

    Add array and isValid function

--- apps/Charge Account Validation/Charge Account Validation/Form1.vb
+++ apps/Charge Account Validation/Charge Account Validation/Form1.vb
@@ -5,6 +5,11 @@ Option Explicit On
 
 Public Class Form1
 
+    ' Declar array of valid numbers 
+    Dim intNumbers() As Integer = {5658845, 4520125, 7895122, 8777541, 8451277, 1302850,
+                                   8080152, 4562555, 5552012, 5050552, 7825877, 1250255,
+                                   1005231, 6545231, 3852085, 7576651, 7881200, 4581002}
+
     Private Sub btnValidate_Click(sender As Object, e As EventArgs) Handles btnValidate.Click
 
     End Sub
