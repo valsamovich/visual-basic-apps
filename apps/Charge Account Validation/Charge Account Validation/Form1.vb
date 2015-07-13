247001e65caad9c287f68673f058fea939c11686
Valery Samovich <valerysamovich@gmail.com>
2015-07-12 21:53:40 -0400

    Add templates for validate and exit handler events

--- apps/Charge Account Validation/Charge Account Validation/Form1.vb
+++ apps/Charge Account Validation/Charge Account Validation/Form1.vb
@@ -1,3 +1,16 @@
-﻿Public Class Form1
+﻿' Written by Valery Samovich 
+' July 12, 2015
+Option Strict On
+Option Explicit On
 
+Public Class Form1
+
+    Private Sub btnValidate_Click(sender As Object, e As EventArgs) Handles btnValidate.Click
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ' Close the form.
+        Me.Close()
+    End Sub
 End Class
