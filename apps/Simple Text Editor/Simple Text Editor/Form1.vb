058f57a11d6c9c65bdab8f492654158a523ef3d1
Valery Samovich <valerysamovich@gmail.com>
2015-07-16 23:28:25 -0400

    Add OpenDocument procedure

--- apps/Simple Text Editor/Simple Text Editor/Form1.vb
+++ apps/Simple Text Editor/Simple Text Editor/Form1.vb
@@ -15,4 +15,45 @@ Public Class Form1
         ' Set is Changed to False.
         blnIsChanged = False
     End Sub
+
+    ' The OpenDocument procedure opens a file and loads it
+    ' into the TextBox for editing.
+
+    Sub OpenDocument()
+        Dim inputFile As StreamReader   ' Object variable
+
+        If ofdOpenFile.ShowDialog = Windows.Forms.DialogResult.OK Then
+            ' Retieve the elected filename.
+            strFilename = ofdOpenFile.FileName
+
+            Try
+                ' Open the file.
+                inputFile = File.OpenText(strFilename)
+
+                ' Read thr file's contents into the TextBox.
+                txtDocument.Text = inputFile.ReadToEnd
+
+                ' Close the file.
+                inputFile.Close()
+
+                ' Update the isChanged variable.
+                blnIsChanged = False
+            Catch
+                ' Error message for file open error.
+                MessageBox.Show("Error opening the file.")
+            End Try
+        End If
+    End Sub
+
+    ' The SaveDocumnet procedure saves the current document.
+
+    Sub SaveDocument()
+        Dim outputFile As StreamWriter  ' Object variable
+
+        Try
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
 End Class
