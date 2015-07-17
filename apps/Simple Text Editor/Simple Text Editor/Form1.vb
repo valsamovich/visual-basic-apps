1728d4718e4be2f025438282a5863f606bd5226c
Valery Samovich <valerysamovich@gmail.com>
2015-07-16 23:15:51 -0400

    Add process for clean and reset document

--- apps/Simple Text Editor/Simple Text Editor/Form1.vb
+++ apps/Simple Text Editor/Simple Text Editor/Form1.vb
@@ -1,3 +1,18 @@
-﻿Public Class Form1
+﻿Imports System.IO
 
+Public Class Form1
+    ' Class-level variables
+    Private strFilename As String = String.Empty    ' Document filename
+    Dim blnIsChanged As Boolean = False             ' File change flag
+
+    Sub ClearDocumnet()
+        ' Clear the contents of the text box.
+        txtDocument.Clear()
+
+        ' Clear the documnet name.
+        strFilename = String.Empty
+
+        ' Set is Changed to False.
+        blnIsChanged = False
+    End Sub
 End Class
