66acca862383804776c8b55905be68c772120feb
Valery Samovich <valerysamovich@gmail.com>
2015-07-20 00:29:00 -0400

    Add tests score loop

--- apps/All COP1000 Grades/All COP1000 Grades/Form1.vb
+++ apps/All COP1000 Grades/All COP1000 Grades/Form1.vb
@@ -3,12 +3,14 @@
 Option Strict On
 Option Explicit On
 
-Public Class Form1
+Imports System.IO
 
+Public Class Form1
     ' Class-level constants for total possible points per category
-    Const intTEST_POSSIBLE_POINTS As Integer = 450       ' Possible points per one test
-    Const intTUTORIAL_POSSIBLE_POINTS As Integer = 175   ' Possible points per one tutorial
-    Const intPROJECT_POSSIBLE_POINTS As Integer = 280    ' Possible points per one project
+    Const intTEST_POSSIBLE_POINTS As Integer = 450      ' Possible points per one test
+    Const intTUTORIAL_POSSIBLE_POINTS As Integer = 175  ' Possible points per one tutorial
+    Const intPROJECT_POSSIBLE_POINTS As Integer = 280   ' Possible points per one project
+    Const strFILENAME As String = "MyGrades.txt"        ' To store scores
 
     ' Array declarations for three categories of scores.
     Dim intTests(7) As Integer       ' Hold the 8 tests scores
@@ -21,11 +23,30 @@ Public Class Form1
     End Function
 
     Private Sub bntEnterScores_Click(sender As Object, e As EventArgs) Handles bntEnterScores.Click
+        Dim intCount As Integer = 0     ' Loop counter, intialized to 0
+        Dim intNumber As Integer        ' To hold user input
 
+        ' Get the scores and add them to the array
+        Do While intCount < intTests.Length
+            Try
+                ' Get and tests scores
+                intNumber = CInt(InputBox("Enter score for test #1."))
+
+                ' Add the score to the array
+                'TO-DO
+
+                ' Increment intCount
+                intCount += 1
+            Catch
+                ' Error messagge for invalid integer.
+                MessageBox.Show("Enter a numeric value for test score.")
+            End Try
+        Loop
     End Sub
 
     Private Sub btnSaveScores_Click(sender As Object, e As EventArgs) Handles btnSaveScores.Click
-
+        ' Local variables
+        Dim outputFile As StreamWriter  ' Object variable
     End Sub
 
     Private Sub btnLoadGrades_Click(sender As Object, e As EventArgs) Handles btnLoadGrades.Click
