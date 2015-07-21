ff0d245f448f93ad0a093b7de6938f889384c21b
Valery Samovich <valerysamovich@gmail.com>
2015-07-20 23:37:57 -0400

    Add functionality to store array values in file

--- apps/All COP1000 Grades/All COP1000 Grades/Form1.vb
+++ apps/All COP1000 Grades/All COP1000 Grades/Form1.vb
@@ -10,54 +10,62 @@ Public Class Form1
     Const intTEST_POSSIBLE_POINTS As Integer = 450      ' Possible points per one test
     Const intTUTORIAL_POSSIBLE_POINTS As Integer = 175  ' Possible points per one tutorial
     Const intPROJECT_POSSIBLE_POINTS As Integer = 280   ' Possible points per one project
-    ' Const strFILENAME As String = "MyGrades.txt"        ' To store scores
-
-    Dim 
 
     ' Array declarations for three categories of scores.
-    Dim intTests(7) As Integer       ' Hold the 8 tests scores
-    Dim intTutorials(6) As Integer   ' Hold the 7 tutorials scores
-    Dim intProjects(6) As Integer    ' Hold the 7 projects scores
+    Dim intTestsScores(7) As Integer       ' Hold the 8 tests scores
+    Dim intTutorialsScores(6) As Integer   ' Hold the 7 tutorials scores
+    Dim intProjectsScores(6) As Integer    ' Hold the 7 projects scores
 
     ' Function drop lowest score and calculate the total nuber of points. 
-    ' Function ProcessScores() As Integer
-
-    ' End Function
+    Function ProcessScores() As Integer
+        ' TO-DO: Add body for this function. 
+    End Function
 
     Private Sub bntEnterScores_Click(sender As Object, e As EventArgs) Handles bntEnterScores.Click
         Dim intTestCount As Integer         ' Loop counter
         Dim intTestScore As Integer         ' To hold the user's input
         Dim intTestIndex As Integer         ' Array index variable
 
-        ' Get 7 scores and add them to the test array.
-        For intTestCount = 1 To 7
-            Try
+        Try
+            ' Get 7 scores and add them to the test array.
+            For intTestCount = 1 To 7
+
                 ' Get a test scores.
                 intTestScore = CInt(InputBox("Enter score test #" & CStr(intTestCount)))
                 ' Add it to the test array.
-                intTests(intTestIndex) = intTestScore
-            Catch
-                ' Error messagge for invalid score.
-                MessageBox.Show("Enter a numeric value for test score.")
-            End Try
-        Next
+                intTestsScores(intTestIndex) = intTestScore
+
+            Next
+        Catch
+            ' Error message for invalid score.
+            MessageBox.Show("Enter a numeric value for test score.")
+        End Try
 
     End Sub
 
     Private Sub btnSaveScores_Click(sender As Object, e As EventArgs) Handles btnSaveScores.Click
-        ' Write array elements one per line
-        For Each Score As 
+        ' Local variables.
+        Dim outputFile As StreamWriter      ' Object variable
+        Dim intTestCount As Integer         ' Hold the count for tests
 
-            ' Local variables.
-            Dim outputFile As StreamWriter  ' Object variable
-
+        Try
+            ' Add test scores to the file.
+            outputFile = File.CreateText("MyScores.txt")
+            For intTestCount = 0 To (intTestsScores.Length - 1)
+                outputFile.WriteLine(intTestsScores(intTestCount))
+            Next
+            outputFile.Close()
+        Catch
+            ' Error message for invalid score.
+            MessageBox.Show("No scores have been entered. Click Enter Scores button to enter your scores.")
+        End Try
     End Sub
 
     Private Sub btnLoadScores_Click(sender As Object, e As EventArgs) Handles btnLoadScores.Click
         ' Local variables.
         Dim inputFile As StreamReader       ' Object variable
-        Dim intTestCount As Integer        ' Hold the count for tests
-        Dim intTestScore As Integer            ' Hold the list number
+        Dim intTestCount As Integer         ' Hold the count for tests
+        Dim intTestScore As Integer         ' Hold the list number
         ' TO-DO: Add local variables for tutorials.
         ' TO-DO: Add local variables for projects.
 
@@ -66,7 +74,7 @@ Public Class Form1
 
         Try
             ' Open the file.
-            inputFile = File.OpenText(strFILENAME)
+            inputFile = File.OpenText("MyGrades.txt")
 
             ' Clear the listboxes.
             lstTests.Items.Clear()
@@ -86,7 +94,7 @@ Public Class Form1
             inputFile.Close()
         Catch
             ' Error message for file open error.
-            MessageBox.Show(strFILENAME & "cannot be opened.")
+            MessageBox.Show("No scores have been entered. Click Enter Scores button to enter your scores.")
         End Try
     End Sub
 
